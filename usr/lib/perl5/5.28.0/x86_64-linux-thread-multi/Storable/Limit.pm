a7ca8075de81dde1adbe3485918b5abe1ffa1a04
Darren Wu <darren19970810@gmail.com>
2019-03-04 21:37:05 +0800

    6.40. Perl-5.28.0

---
+++ usr/lib/perl5/5.28.0/x86_64-linux-thread-multi/Storable/Limit.pm
@@ -0,0 +1,6 @@
+# bisected by stacksize
+$Storable::recursion_limit = 14265
+  unless defined $Storable::recursion_limit;
+$Storable::recursion_limit_hash = 8552
+  unless defined $Storable::recursion_limit_hash;
+1;
